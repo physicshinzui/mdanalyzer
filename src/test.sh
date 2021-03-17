aa29288f97678841a4043374f6c37a118478c538
physicshinzui@gmail.com <siida@Shinjis-MacBook-Air.local>
2021-03-17 17:50:53 +0900

    test.sh was updated so that it can test the version that handles a reference which can be not the same as the one for trajectory loading.

--- src/test.sh
+++ src/test.sh
@@ -3,3 +3,4 @@ set -Ceu
 
 python mdanalyzer.py -r ../../00_samples/sample2/ref.pdb \
                      -i ../../00_samples/sample2/traj_aligned.xtc \
+                     --rmsd_ref /Users/siida/Dropbox/01code/software/myTools/00_samples/sample2/em2.gro 
